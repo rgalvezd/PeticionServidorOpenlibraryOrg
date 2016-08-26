b057ede75409b329166c747110504d06559d3423
Rodrigo Galvez <rodrigogalvezdiaz@outlook.com>
2016-08-26 16:50:15 +0200

    Initial Commit

---
+++ AccederWebOpenLibraryOrg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccederWebOpenLibraryOrg
+//
+//  Created by Rodrigo Gálvez Díaz on 26/8/16.
+//  Copyright © 2016 iOS.SwiftProgramar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
